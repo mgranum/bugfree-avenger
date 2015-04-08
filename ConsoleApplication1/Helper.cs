556fd0004dddc72826edcdae7b910781b71d93c5
mgr <mgranum@mac.com>
2015-04-08 16:39:22 +0000

    CreateTick refactored to AdjustTime

--- ConsoleApplication1/Helper.cs
+++ ConsoleApplication1/Helper.cs
@@ -4,7 +4,7 @@ namespace ConsoleApplication1
 {
     public static class Helper
     {
-        public static Tick CreateTick(DateTime dateTime, double price, int timeInterval)
+        public static DateTime AdjustTime(DateTime dateTime, int timeInterval)
         {
             if (timeInterval == 1)
             {
@@ -15,9 +15,7 @@ namespace ConsoleApplication1
                 dateTime = dateTime.AddMinutes(-(dateTime.Minute % timeInterval)).AddSeconds(-(dateTime.Second));
             }
 
-            var tick = new Tick() { Timestamp = dateTime };
-
-            return tick;
+            return dateTime;
             //dt = dt.AddMinutes(-(dt.Minute % 15)).AddSeconds(-dt.Second);
             //dateTime = dateTime.AddMinutes(-(dateTime))
             //throw new NotImplementedException();
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -26,48 +26,48 @@ namespace UnitTestProject1
         [TestMethod]
         public void CreateTick_WhenIntervalIsZero_TimeRemainsUnchanged()
         {
-            var tick = Helper.CreateTick(timestamp, 1.0, 0);
-            Assert.AreEqual(timestamp, tick.Timestamp);
+            var tick = Helper.AdjustTime(timestamp, 0);
+            Assert.AreEqual(timestamp, tick);
         }
 
         [TestMethod]
         public void CreateTick_WhenIntervalIsOne_TimeIsRoundedDownToNearestMinute()
         {
-            var tick = Helper.CreateTick(timestamp, 1.0, 1);
+            var tick = Helper.AdjustTime(timestamp, 1);
             var expected = new DateTime(1999, 12, 31, 23, 59, 0);
-            Assert.AreEqual(expected, tick.Timestamp);
+            Assert.AreEqual(expected, tick);
         }
 
         [TestMethod]
         public void CreateTick_WhenIntervalIsFive_TimeIsRoundedDownToNearestFive()
         {
-            var tick = Helper.CreateTick(timestamp, 1.0, 5);
+            var tick = Helper.AdjustTime(timestamp, 5);
             var expected = new DateTime(1999, 12, 31, 23, 55, 0);
-            Assert.AreEqual(expected, tick.Timestamp);
+            Assert.AreEqual(expected, tick);
         }
 
         [TestMethod]
         public void CreateTick_WhenIntervalIsFifteen_TimeIsRoundedDownToNearestQuarter()
         {
-            var tick = Helper.CreateTick(timestamp, 1.0, 15);
+            var tick = Helper.AdjustTime(timestamp, 15);
             var expected = new DateTime(1999, 12, 31, 23, 45, 0);
-            Assert.AreEqual(expected, tick.Timestamp);
+            Assert.AreEqual(expected, tick);
         }
 
         [TestMethod]
         public void CreateTick_WhenIntervalIsThirty_TimeIsRoundedDownToNearestHalfHour()
         {
-            var tick = Helper.CreateTick(timestamp, 1.0, 30);
+            var tick = Helper.AdjustTime(timestamp, 30);
             var expected = new DateTime(1999, 12, 31, 23, 30, 0);
-            Assert.AreEqual(expected, tick.Timestamp);
+            Assert.AreEqual(expected, tick);
         }
 
         [TestMethod]
         public void CreateTick_WhenIntervalIsSixty_TimeIsRoundedDownToNearestWholeHour()
         {
-            var tick = Helper.CreateTick(timestamp, 1.0, 60);
+            var tick = Helper.AdjustTime(timestamp, 60);
             var expected = new DateTime(1999, 12, 31, 23, 0, 0);
-            Assert.AreEqual(expected, tick.Timestamp);
+            Assert.AreEqual(expected, tick);
         }
     }
 }
