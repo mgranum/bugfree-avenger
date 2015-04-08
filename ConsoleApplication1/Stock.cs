64c413097ba429de993a5db7e5f06775c6237a23
mgr <mgranum@mac.com>
2015-04-08 19:50:47 +0000

    AddOrUpdateTick to accumulate data

--- ConsoleApplication1/Stock.cs
+++ ConsoleApplication1/Stock.cs
@@ -8,24 +8,64 @@ namespace ConsoleApplication1
 {
     public class Stock
     {
+        private List<Tick> list01;
+        private List<Tick> list05;
+        private List<Tick> list15;
+
         public string Ticker { get; set; }
-        public List<Tick> List01 { get; }
-        public List<Tick> List05 { get; }
-        public List<Tick> List15 { get; }
+        public List<Tick> List05 { get { return list05; } }
+        public List<Tick> List15 { get { return list15; } }
+        public List<Tick> List01
+        {
+            get { return list01; }
+        }
+
 
         public Stock()
         {
-            List01 = new List<Tick>();
-            List05 = new List<Tick>();
-            List15 = new List<Tick>();
+            list01 = new List<Tick>();
+            list05 = new List<Tick>();
+            list15 = new List<Tick>();
         }
 
         public void AddTrade(DateTime timestamp, double price)
         {
             var dt01 = Helper.AdjustTime(timestamp, 1);
-            var tick = new Tick() { Timestamp = dt01 };
+            AddOrUpdateTick(ref list01, dt01, price);
 
-            List01.Add(tick);
+            var dt05 = Helper.AdjustTime(timestamp, 5);
+            AddOrUpdateTick(ref list05, dt05, price);
+
+            var dt15 = Helper.AdjustTime(timestamp, 15);
+            AddOrUpdateTick(ref list15, dt15, price);
+        }
+
+        public void AddOrUpdateTick(ref List<Tick> list, DateTime dateTime, double price)
+        {
+            var existingTick = list.Find(x => x.Timestamp == dateTime);
+            if (existingTick != null)
+            {
+                if (price < existingTick.Low)
+                {
+                    existingTick.Low = price;
+                }
+                if (price > existingTick.High)
+                {
+                    existingTick.High = price;
+                }
+                existingTick.Close = price;
+            }
+            else
+            {
+                var tick = new Tick();
+                tick.Timestamp = dateTime;
+                tick.Open = price;
+                tick.Close = price;
+                tick.High = price;
+                tick.Low = price;
+
+                list.Add(tick);
+            }
         }
     }
 }
--- UnitTestProject1/StockTests.cs
+++ UnitTestProject1/StockTests.cs
@@ -9,12 +9,16 @@ namespace UnitTestProject1
     {
         private Stock stock;
         private DateTime timestamp;
+        private DateTime dt01, dt05, dt15;
 
         [TestInitialize]
         public void Setup()
         {
             stock = new Stock();
             timestamp = new DateTime(1999, 12, 31, 23, 59, 59);
+            dt01 = new DateTime(1999, 12, 31, 23, 59, 0);
+            dt05 = new DateTime(1999, 12, 31, 23, 55, 0);
+            dt15 = new DateTime(1999, 12, 31, 23, 45, 0);
         }
 
         [TestMethod]
@@ -30,6 +34,75 @@ namespace UnitTestProject1
         {
             stock.AddTrade(timestamp, 1.0);
             Assert.AreEqual(1, stock.List01.Count);
+            Assert.AreEqual(1, stock.List05.Count);
+            Assert.AreEqual(1, stock.List15.Count);
+        }
+
+        [TestMethod]
+        public void AddTrade_WhenAddingOneTrade_TimesAreAdjustedInLists()
+        {
+            stock.AddTrade(timestamp, 1.0);
+            Assert.AreEqual(dt01, stock.List01[0].Timestamp);
+            Assert.AreEqual(dt05, stock.List05[0].Timestamp);
+            Assert.AreEqual(dt15, stock.List15[0].Timestamp);
+        }
+
+        [TestMethod]
+        public void AddTrade_WhenAddingOneTrade_AllAmountsAreIdenticalInLists()
+        {
+            stock.AddTrade(timestamp, 1.23);
+            Assert.AreEqual(1.23, stock.List01[0].Open);
+            Assert.AreEqual(1.23, stock.List01[0].Close);
+            Assert.AreEqual(1.23, stock.List01[0].High);
+            Assert.AreEqual(1.23, stock.List01[0].Low);
+
+            Assert.AreEqual(1.23, stock.List05[0].Open);
+            Assert.AreEqual(1.23, stock.List05[0].Close);
+            Assert.AreEqual(1.23, stock.List05[0].High);
+            Assert.AreEqual(1.23, stock.List05[0].Low);
+
+            Assert.AreEqual(1.23, stock.List15[0].Open);
+            Assert.AreEqual(1.23, stock.List15[0].Close);
+            Assert.AreEqual(1.23, stock.List15[0].High);
+            Assert.AreEqual(1.23, stock.List15[0].Low);
+        }
+
+        [TestMethod]
+        public void AddTrade_WhenAddingTwoTradesWithSameTime_ListJustContainsOneItem()
+        {
+            stock.AddTrade(timestamp, 1.0);
+            stock.AddTrade(timestamp, 1.0);
+            Assert.AreEqual(1, stock.List01.Count);
+            Assert.AreEqual(1, stock.List05.Count);
+            Assert.AreEqual(1, stock.List15.Count);
+        }
+
+        [TestMethod]
+        public void AddTrade_WhenAddingTwoTradesWithSameTime_ListContainsOneItemWithCorrectValues()
+        {
+            stock.AddTrade(timestamp, 2.34);
+            stock.AddTrade(timestamp, 1.23);
+            Assert.AreEqual(2.34, stock.List01[0].High);
+            Assert.AreEqual(1.23, stock.List01[0].Low);
+            Assert.AreEqual(2.34, stock.List01[0].Open);
+            Assert.AreEqual(1.23, stock.List01[0].Close);
+
+            Assert.AreEqual(2.34, stock.List05[0].High);
+            Assert.AreEqual(1.23, stock.List05[0].Low);
+            Assert.AreEqual(2.34, stock.List05[0].Open);
+            Assert.AreEqual(1.23, stock.List05[0].Close);
+
+            Assert.AreEqual(2.34, stock.List15[0].High);
+            Assert.AreEqual(1.23, stock.List15[0].Low);
+            Assert.AreEqual(2.34, stock.List15[0].Open);
+            Assert.AreEqual(1.23, stock.List15[0].Close);
+
+        }
+
+        [TestCleanup]
+        public void Teardown()
+        {
+
         }
     }
 }
