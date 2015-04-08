5467049cc3e00e7a334d2f3396462edf52c1c97a
mgr <mgranum@mac.com>
2015-04-08 18:04:01 +0000

    Added Stock class

---
+++ ConsoleApplication1/Stock.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Stock
+    {
+        public string Ticker { get; set; }
+        public List<Tick> List01 { get; }
+        public List<Tick> List05 { get; }
+        public List<Tick> List15 { get; }
+
+        public Stock()
+        {
+            List01 = new List<Tick>();
+            List05 = new List<Tick>();
+            List15 = new List<Tick>();
+        }
+
+        public bool AddTrade(DateTime timestamp, double price)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
